05b13076ed4397adc05a8c4ddf4f94d9cbb175ac
guilhermepita-dotcom <guilherme.pita@souzaemonteiro.com.br>
2025-08-23 14:32:21 -0300

    Update estagiarios_app.py

--- estagiarios_app.py
+++ estagiarios_app.py
@@ -77,7 +77,7 @@ def load_custom_css():
             :root {
                 --primary-color: #FFFFFF;
                 --background-color: #faf9f7;
-                --secondary-background-color: #E2A144;
+                --secondary-background-color: #8c8c8b;
                 --text-color: #FFFFFF;
                 --text-color-muted: #E2A144;
                 --font-family: 'Poppins', sans-serif;
@@ -669,6 +669,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
